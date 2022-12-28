c6f7b5365a857e3c0509f19f1276ed405ae55169
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-12-28 12:35:56 -0500

    Add files via upload

--- Ch 3/ch_3.sql
+++ Ch 3/ch_3.sql
@@ -91,9 +91,16 @@ FROM teachers WHERE first_name ILIKE 'sam%';
 
 -- Combining Operators with AND and OR
 
+SELECT *
+FROM teachers WHERE school = 'Myers Middle School' AND salary < 40000;
+SELECT *
+FROM teachers WHERE last_name = 'Cole' OR last_name = 'Bush';
+SELECT *
+FROM teachers WHERE school = 'F.D. Roosevelt HS' AND (salary < 38000 Or salary > 40000);
 
+-- Putting It All Together
 
-
+-- DeBarros, Anthony. Practical SQL, 2nd Edition (p. 39). No Starch Press. Kindle Edition. 
 
 
 
