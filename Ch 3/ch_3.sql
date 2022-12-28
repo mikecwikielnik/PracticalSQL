e1671f950ffe6759f0c7ace2379958277a13babb
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-12-28 10:37:59 -0500

    Add files via upload

--- Ch 3/ch_3.sql
+++ Ch 3/ch_3.sql
@@ -8,6 +8,13 @@ SELECT * FROM teachers;
 
 -- a quick way to view the tables
 
-TABLE type_of_animal;
+TABLE type_of_animal; -- neato
 
 -- querying a subset of columns
+
+SELECT last_name, first_name, salary FROM teachers;
+
+-- Sorting Data with ORDER BY
+
+-- DeBarros, Anthony. Practical SQL, 2nd Edition (p. 32). No Starch Press. Kindle Edition. 
+
