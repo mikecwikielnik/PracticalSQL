bc4b39dc4246aa0c572f77f3779b835960aff3f1
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-12-28 11:17:34 -0500

    Add files via upload

--- Ch 3/ch_3.sql
+++ Ch 3/ch_3.sql
@@ -18,3 +18,35 @@ SELECT last_name, first_name, salary FROM teachers;
 
 -- DeBarros, Anthony. Practical SQL, 2nd Edition (p. 32). No Starch Press. Kindle Edition. 
 
+SELECT first_name, last_name, salary
+FROM teachers
+ORDER BY salary DESC;
+
+-- you can use numbers to id columns as well
+
+SELECT first_name, last_name, salary
+FROM teachers
+ORDER BY 3 DESC;
+
+--- ex: sorting on more than one column
+-- this shows us the newest teachers at each school! 
+
+SELECT last_name, school, hire_Date
+FROM teachers
+ORDER BY school ASC,hire_date DESC;
+
+-- Using DISTINCT to Find Unique Values
+
+-- DeBarros, Anthony. Practical SQL, 2nd Edition (p. 33). No Starch Press. Kindle Edition. 
+
+
+
+
+
+
+
+
+
+
+
+
