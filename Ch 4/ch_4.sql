10ac8bcd5ccc8a4a6b9e794ca194191ddcf32027
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-01-05 12:19:35 -0500

    Add files via upload

--- Ch 4/ch_4.sql
+++ Ch 4/ch_4.sql
@@ -45,12 +45,20 @@ SELECT numeric_column*10000000 AS fixed, real_column*10000000 AS floating
 FROM number_data_types
 WHERE numeric_column = .7;
 
+-- Listing 4-4: The timestamp and interval types in action
 
+-- DeBarros, Anthony. Practical SQL, 2nd Edition (p. 52). No Starch Press. Kindle Edition. 
 
+CREATE TABLE date_time_types (timestamp_column timestamp with time zone, 
+							  interval_column interval);
+INSERT INTO date_time_types
+VALUES ('2022-12-31 01:00 EST','2 days'), ('2022-12-31 01:00 -8','1 month'), ('2022-12-31 01:00 Australia/Melbourne','1 century'), (now(),'1 week');
 
+SELECT * FROM date_time_types;
 
+-- Listing 4-5: Using the interval data type
 
-
+-- DeBarros, Anthony. Practical SQL, 2nd Edition (p. 53). No Starch Press. Kindle Edition. 
 
 
 
