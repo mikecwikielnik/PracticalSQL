d7da822545baeaf817f08c5ade5bb8f99ecf1549
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-01-18 11:01:44 -0500

    Add files via upload

---
+++ Ch 5/ch_5.sql
@@ -0,0 +1,14 @@
+-- Importing and Exporting Data
+
+-- DeBarros, Anthony. Practical SQL, 2nd Edition (p. 59). No Starch Press. Kindle Edition. 
+
+-- Using COPY to Import Data
+
+-- DeBarros, Anthony. Practical SQL, 2nd Edition (p. 62). No Starch Press. Kindle Edition. 
+
+-- COPY table_name
+-- FROM 'C:/YourDirectory/your_file.csv' WITH (FORMAT CSV, HEADER);
+
+-- Importing Census Data Describing Counties
+
+-- DeBarros, Anthony. Practical SQL, 2nd Edition (p. 63). No Starch Press. Kindle Edition. 
