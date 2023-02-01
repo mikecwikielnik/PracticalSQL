589e40d26695cde8fc1077c9c5d4c65cd23401bd
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-02-01 11:28:01 -0500

    Add files via upload

---
+++ Ch 6/ch_6.sql
@@ -0,0 +1,56 @@
+-- Basic Math and Stats with SQL
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 3027). Kindle Edition. 
+
+-- Listing 6-1: Basic addition, subtraction, and multiplication with SQL
+
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 3083-3084). Kindle Edition. 
+
+
+SELECT 2 + 2;
+SELECT 9 - 1;
+SELECT 3 * 4.0;
+
+SELECT 5 * 5 AS result;
+
+-- Listing 6-2: Integer and decimal division with SQL
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 3100). Kindle Edition. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
