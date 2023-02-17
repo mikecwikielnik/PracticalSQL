a714bf4a10320abbe1f5af01ce62077233728a24
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-02-16 23:46:56 -0500

    Add files via upload

--- Ch 6/ch_6.sql
+++ Ch 6/ch_6.sql
@@ -82,7 +82,7 @@ ORDER BY difference DESC;
 
 SELECT county_name AS county,
 	state_name AS state,
-	area_water::numeric/(area_land + area_water)*100 AS pct_water
+	area_water::numeric/(area_land + area_water) * 100 AS pct_water
 FROM us_counties_pop_est_2019
 ORDER BY pct_water DESC;
 
@@ -90,6 +90,30 @@ ORDER BY pct_water DESC;
 
 -- Anthony DeBarros. 9781718501072 (Kindle Location 3268). Kindle Edition. 
 
+CREATE TABLE percent_change(
+	department text,
+	spend_2019 numeric(10,2),
+	spend_2022 numeric(10,2)
+);
+
+INSERT INTO percent_change
+	VALUES
+	('Assessor', 178556, 179500),
+	('Building', 250000, 289000),
+	('Clerk', 451980, 650000),
+	('Library', 87777, 90001),
+	('Parks', 250000, 223000),
+	('Water', 199000, 195000);
+	
+SELECT department,
+	spend_2019,
+	spend_2022,
+	round((spend_2022 - spend_2019) / spend_2019 * 100, 1) AS pct_change
+FROM percent_change;
+
+-- Listing 6-9: Using the sum() and avg() aggregate functions
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 3295). Kindle Edition. 
 
 
 
