1a316581a2fdfdfcacfc87693034598a589f87d0
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-02-20 17:25:53 -0500

    Add files via upload

--- Ch 6/ch_6.sql
+++ Ch 6/ch_6.sql
@@ -133,15 +133,60 @@ INSERT INTO percentile_test(numbers) VALUES
 	(1), (2), (3), (4), (5), (6);
 	
 SELECT 
-percentile_cont(.5)
-WITHIN GROUP (ORDER BY numbers),
-percentile_disc(.5)
-WITHIN GROUP (ORDER BY numbers)
+	percentile_cont(.5)
+	WITHIN GROUP (ORDER BY numbers),
+	percentile_disc(.5)
+	WITHIN GROUP (ORDER BY numbers)
 FROM percentile_test;
 
 -- Listing 6-11: Using sum(), avg(), and percentile_cont() aggregate functions
 
 -- Anthony DeBarros. 9781718501072 (Kindle Locations 3363-3364). Kindle Edition. 
+
+SELECT sum(pop_est_2019) AS county_sum,
+	round(avg(pop_est_2019), 0) AS county_average,
+	percentile_cont(.5)
+	WITHIN GROUP (ORDER BY pop_est_2019) AS county_median
+FROM us_counties_pop_est_2019;
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
