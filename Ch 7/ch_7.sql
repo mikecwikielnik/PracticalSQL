66ea39bb584208efed6bb4c5dcc50d1157ead0ba
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-02-26 22:00:36 -0500

    Add files via upload

--- Ch 7/ch_7.sql
+++ Ch 7/ch_7.sql
@@ -111,11 +111,18 @@ ORDER BY district_2035.id;
 
 -- Anthony DeBarros. 9781718501072 (Kindle Location 3686). Kindle Edition. 
 
+SELECT *
+FROM district_2020 FULL OUTER JOIN district_2035
+ON district_2020.id = district_2035.id
+ORDER BY district_2020.id;
 
+-- Listing 7-9: Using CROSS JOIN
 
+-- Anthony DeBarros. 9781718501072 (Kindle Location 3703). Kindle Edition. 
 
-
-
+SELECT * 
+FROM district_2020 CROSS JOIN district_2035
+ORDER BY district_2020.id, district_2035.id;
 
 
 
