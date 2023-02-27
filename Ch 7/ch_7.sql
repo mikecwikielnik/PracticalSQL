64ed0efb4b05ab3985a35617683334160f552727
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-02-26 21:15:17 -0500

    Add files via upload

--- Ch 7/ch_7.sql
+++ Ch 7/ch_7.sql
@@ -44,6 +44,58 @@ FROM employees JOIN departments
 ON employees.dept_id = departments.dept_id
 ORDER BY employees.dept_id;
 
+-- Listing 7-3: Creating two tables to explore JOIN types
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 3608). Kindle Edition. 
+
+CREATE TABLE district_2020 (
+	id integer CONSTRAINT id_key_2020 PRIMARY KEY,
+	school_2020 text
+);
+
+CREATE TABLE district_2035 (
+	id integer CONSTRAINT id_key_2035 PRIMARY KEY,
+	school_2035 text
+);
+
+INSERT INTO district_2020 VALUES
+	(1, 'Oak Street School'),
+	(2, 'Roosevelt High School'),
+	(5, 'Dover Middle School'),
+	(6, 'Webutuck High School');
+	
+INSERT INTO district_2035 VALUES
+	(1, 'Oak Street School'),
+	(2, 'Roosevelt High School'),
+	(3, 'Morrison Elementary'),
+	(4, 'Chase Magnet Academy'),
+	(6, 'Webutuck High School');
+	
+-- Listing 7-4: Using JOIN
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 3621). Kindle Edition. 
+
+SELECT *
+FROM district_2020 JOIN district_2035
+ON district_2020.id = district_2035.id 	-- the on statement col have to match
+ORDER BY district_2020.id
+
+-- Listing 7-5: JOIN with USING
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 3638). Kindle Edition. 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
