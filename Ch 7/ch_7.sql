d7ade138efe2c5c00cafcda160f4dcb4ca610c79
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-02-27 19:35:41 -0500

    Add files via upload

--- Ch 7/ch_7.sql
+++ Ch 7/ch_7.sql
@@ -226,6 +226,25 @@ SELECT '2035' as year,
 FROM district_2035
 ORDER BY school, year;
 
+-- Listing 7-17: Combining query results with INTERSECT and EXCEPT
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 3919). Kindle Edition. 
+
+SELECT * FROM district_2020
+INTERSECT 
+SELECT * FROM district_2035
+ORDER BY id;
+
+SELECT * FROM district_2020
+EXCEPT
+SELECT * FROM district_2035
+ORDER BY id;
+
+
+
+
+
+
 
 
 
