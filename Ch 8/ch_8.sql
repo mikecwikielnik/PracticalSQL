6c888074b435db302917b2e492263a2a88035e10
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-03-04 11:57:52 -0500

    Add files via upload

--- Ch 8/ch_8.sql
+++ Ch 8/ch_8.sql
@@ -56,9 +56,23 @@ VALUES (775, '2022-01-23', 'Y');
 INSERT INTO natural_key_composite_example (student_id, school_day, present)
 VALUES (775, '2022-01-23', 'N');
 
+-- Listing 8-5: Declaring a bigint column as a surrogate key using IDENTITY
 
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 4229-4230). Kindle Edition. 
 
+CREATE TABLE surrogate_key_example (
+	order_number bigint GENERATED ALWAYS AS IDENTITY,
+	product_name text,
+	order_time timestamp with time zone,
+	CONSTRAINT order_number_key PRIMARY KEY (order_number)
+);
 
+INSERT INTO surrogate_key_example (product_name, order_time)
+VALUES  ('Beachball Polish', '2020-03-15 09:21-07'),
+		('Wrinkle De-Atomizer', '2017-05-22 14:00-07'),
+		('Flux Capacitor', '1985-10-26 01:18:00-07');
+		
+SELECT * FROM surrogate_key_example;
 
 
 
