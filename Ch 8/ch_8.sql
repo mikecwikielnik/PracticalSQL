7b3ca108955b2b7917892fa30fabd96d57676abd
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-03-11 20:48:05 -0500

    Add files via upload

--- Ch 8/ch_8.sql
+++ Ch 8/ch_8.sql
@@ -89,6 +89,53 @@ VALUES ('Aloe Plant', '2020-03-15 10:09-07');
 
 SELECT * FROM surrogate_key_example;
 
+-- Listing 8-7: A foreign key example
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 4298). Kindle Edition. 
+
+CREATE TABLE licenses (
+	license_id text,
+	first_name text,
+	last_name text,
+	CONSTRAINT licenses_key PRIMARY KEY (license_id)
+);
+
+CREATE TABLE registrations (
+	registration_id text,
+	registration_date timestamp with time zone, 
+	license_id text REFERENCES licenses (license_id),
+	CONSTRAINT registration_key PRIMARY KEY (registration_id, license_id)
+);
+
+INSERT INTO licenses (license_id, first_name, last_name)
+VALUES ('T229901', 'Steve', 'Rothery');
+
+INSERT INTO registrations (registration_id, registration_date, license_id)
+VALUES ('A2033391', '2022-03-17', 'T229901');
+
+INSERT INTO registrations (registration_id, registration_date, license_id)
+VALUES ('A75772', '2022-03-18', 'T000001');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
