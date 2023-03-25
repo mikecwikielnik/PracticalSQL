3cf779892162250c454904df54b74dfb7d9ea850
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-03-25 11:54:02 -0400

    Add files via upload

--- Ch 8/ch_8.sql
+++ Ch 8/ch_8.sql
@@ -133,6 +133,15 @@ CREATE TABLE registrations (
 
 -- Anthony DeBarros. 9781718501072 (Kindle Location 4354). Kindle Edition. 
 
+CREATE TABLE check_constraint_example (
+	user_id bigint GENERATED ALWAYS AS IDENTITY,
+	user_role text,
+	salary numeric(10,2),
+	CONSTRAINT user_id_key PRIMARY KEY (user_id),
+	CONSTRAINT check_role_in_list CHECK (user_role IN('Admin', 'Staff')),
+	CONSTRAINT check_salary_now_below_zero CHECK (salary >= 0)
+);
+
 
 
 
