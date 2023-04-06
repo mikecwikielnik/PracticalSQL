2d8627d3e1707388c36bba6366125a437231dcb9
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-04-05 20:00:43 -0400

    Add files via upload

--- Ch 8/ch_8.sql
+++ Ch 8/ch_8.sql
@@ -142,10 +142,27 @@ CREATE TABLE check_constraint_example (
 	CONSTRAINT check_salary_now_below_zero CHECK (salary >= 0)
 );
 
+-- Listing 8-9: A UNIQUE constraint example
 
+-- Anthony DeBarros. 9781718501072 (Kindle Location 4391). Kindle Edition. 
 
+CREATE TABLE unique_constraint_example (
+	contact_id bigint GENERATED ALWAYS AS IDENTITY,
+	first_name text,
+	last_name text,
+	email text,
+	CONSTRAINT contact_id_key PRIMARY KEY (contact_id),
+	CONSTRAINT email_unique UNIQUE (email)
+);
 
+INSERT INTO unique_constraint_example (first_name, last_name, email)
+VALUES ('Samantha', 'Lee', 'slee@example.org');
 
+INSERT INTO unique_constraint_example (first_name, last_name, email)
+VALUES ('Betty', 'Diaz', 'bdiaz@example.org');
+
+INSERT INTO unique_constraint_example (first_name, last_name, email)
+VALUES ('Sasha', 'Lee', 'slee@example.org');
 
 
 
