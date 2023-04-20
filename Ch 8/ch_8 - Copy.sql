7979e886ab25ff5ef8368a79e306e32516901fa2
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-04-20 19:13:52 -0400

    Add files via upload

---
+++ Ch 8/ch_8 - Copy.sql
@@ -0,0 +1,200 @@
+-- Chapter 8
+-- Table Design That Works for You
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 4002). Kindle Edition. 
+
+-- Listing 8-1: Declaring a single-column natural key as a primary key
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 4160). Kindle Edition. 
+
+CREATE TABLE natural_key_example (
+	license_id text CONSTRAINT license_key PRIMARY KEY,
+	first_name text,
+	last_name text
+);
+
+DROP TABLE natural_key_example;
+
+CREATE TABLE natural_key_example (
+	license_id text,
+	first_name text,
+	last_name text,
+	CONSTRAINT license_key PRIMARY KEY (license_id)
+);
+
+-- Listing 8-2: An example of a primary key violation
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 4179). Kindle Edition. 
+
+INSERT INTO natural_key_example (license_id, first_name, last_name)
+VALUES ('T229901', 'Gem', 'Godfrey');
+
+INSERT INTO natural_key_example (license_id, first_name, last_name)
+VALUES ('T229901', 'John', 'Mitchell');
+
+-- Listing 8-3: Declaring a composite primary key as a natural key
+
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 4196-4197). Kindle Edition. 
+
+CREATE TABLE natural_key_composite_example (
+	student_id text,
+	school_day date,
+	present boolean,
+	CONSTRAINT student_key PRIMARY KEY (student_id, school_day)
+);
+
+-- Listing 8-4: Example of a composite primary key violation
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 4205). Kindle Edition. 
+
+INSERT INTO natural_key_composite_example (student_id, school_day, present)
+VALUES (775, '2022-01-22', 'Y');
+
+INSERT INTO natural_key_composite_example (student_id, school_day, present)
+VALUES (775, '2022-01-23', 'Y');
+
+INSERT INTO natural_key_composite_example (student_id, school_day, present)
+VALUES (775, '2022-01-23', 'N');
+
+-- Listing 8-5: Declaring a bigint column as a surrogate key using IDENTITY
+
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 4229-4230). Kindle Edition. 
+
+CREATE TABLE surrogate_key_example (
+	order_number bigint GENERATED ALWAYS AS IDENTITY,
+	product_name text,
+	order_time timestamp with time zone,
+	CONSTRAINT order_number_key PRIMARY KEY (order_number)
+);
+
+INSERT INTO surrogate_key_example (product_name, order_time)
+VALUES  ('Beachball Polish', '2020-03-15 09:21-07'),
+		('Wrinkle De-Atomizer', '2017-05-22 14:00-07'),
+		('Flux Capacitor', '1985-10-26 01:18:00-07');
+		
+SELECT * FROM surrogate_key_example;
+
+-- Listing 8-6: Restarting an IDENTITY sequence
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 4263). Kindle Edition. 
+
+INSERT INTO surrogate_key_example 
+OVERRIDING SYSTEM VALUE
+VALUES (4, 'Chicken Coop', '2021-09-03 10:33:-07');
+
+ALTER TABLE surrogate_key_example ALTER COLUMN order_number RESTART WITH 5;
+
+INSERT INTO surrogate_key_example (product_name, order_time)
+VALUES ('Aloe Plant', '2020-03-15 10:09-07');
+
+SELECT * FROM surrogate_key_example;
+
+-- Listing 8-7: A foreign key example
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 4298). Kindle Edition. 
+
+CREATE TABLE licenses (
+	license_id text,
+	first_name text,
+	last_name text,
+	CONSTRAINT licenses_key PRIMARY KEY (license_id)
+);
+
+CREATE TABLE registrations (
+	registration_id text,
+	registration_date timestamp with time zone, 
+	license_id text REFERENCES licenses (license_id),
+	CONSTRAINT registration_key PRIMARY KEY (registration_id, license_id)
+);
+
+INSERT INTO licenses (license_id, first_name, last_name)
+VALUES ('T229901', 'Steve', 'Rothery');
+
+INSERT INTO registrations (registration_id, registration_date, license_id)
+VALUES ('A2033391', '2022-03-17', 'T229901');
+
+INSERT INTO registrations (registration_id, registration_date, license_id)
+VALUES ('A75772', '2022-03-18', 'T000001');
+
+-- modify the Listing 8-7 CREATE TABLE statement for registrations, 
+-- adding the keywords at the end of the definition of the license_id column:
+
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 4329-4330). Kindle Edition. 
+
+
+CREATE TABLE registrations (
+	registration_id text,
+	registration_date date,
+	license_id text REFERENCES licenses (license_id) ON DELETE CASCADE,
+	CONSTRAINT registration_key PRIMARY KEY (registration_id, license_id)
+);
+
+-- Listing 8-8: Examples of CHECK constraints
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 4354). Kindle Edition. 
+
+CREATE TABLE check_constraint_example (
+	user_id bigint GENERATED ALWAYS AS IDENTITY,
+	user_role text,
+	salary numeric(10,2),
+	CONSTRAINT user_id_key PRIMARY KEY (user_id),
+	CONSTRAINT check_role_in_list CHECK (user_role IN('Admin', 'Staff')),
+	CONSTRAINT check_salary_now_below_zero CHECK (salary >= 0)
+);
+
+-- Listing 8-9: A UNIQUE constraint example
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 4391). Kindle Edition. 
+
+CREATE TABLE unique_constraint_example (
+	contact_id bigint GENERATED ALWAYS AS IDENTITY,
+	first_name text,
+	last_name text,
+	email text,
+	CONSTRAINT contact_id_key PRIMARY KEY (contact_id),
+	CONSTRAINT email_unique UNIQUE (email)
+);
+
+INSERT INTO unique_constraint_example (first_name, last_name, email)
+VALUES ('Samantha', 'Lee', 'slee@example.org');
+
+INSERT INTO unique_constraint_example (first_name, last_name, email)
+VALUES ('Betty', 'Diaz', 'bdiaz@example.org');
+
+INSERT INTO unique_constraint_example (first_name, last_name, email)
+VALUES ('Sasha', 'Lee', 'slee@example.org');
+
+-- Listing 8-10: A NOT NULL constraint example
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 4410). Kindle Edition. 
+
+CREATE TABLE not_null_example (
+	student_id bigint GENERATED ALWAYS AS IDENTITY,
+	first_name text NOT NULL,
+	last_name text NOT NULL,
+	CONSTRAINT student_id_key PRIMARY KEY (student_id)
+);
+
+-- Listing 8-11: Dropping and adding a primary key and a NOT NULL constraint
+
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 4428-4429). Kindle Edition. 
+
+ALTER TABLE not_null_example DROP CONSTRAINT student_id_key;
+ALTER TABLE not_null_example ADD CONSTRAINT student_id_key PRIMARY KEY (student_id);
+ALTER TABLE not_null_example ALTER COLUMN first_name DROP NOT NULL;
+ALTER TABLE not_null_example ALTER COLUMN first_name SET NOT NULL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
