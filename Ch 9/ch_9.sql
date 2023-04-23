e5b670345095365e3316c74d409aed014dc643c0
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-04-23 17:01:16 -0400

    Add files via upload

--- Ch 9/ch_9.sql
+++ Ch 9/ch_9.sql
@@ -251,4 +251,25 @@ FROM pls_fy2018_libraries
 GROUP BY stabr, stataddr
 ORDER BY stabr, stataddr;
 
+-- Listing 9-11: Using the sum() aggregate function to total visits to libraries in 2016, 2017, and 2018
+
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 4856-4857). Kindle Edition. 
+
+SELECT sum(visits) AS visits_2018
+FROM pls_fy2018_libraries
+WHERE visits >= 0;
+
+SELECT sum(visits) AS visits_2017
+FROM pls_fy2017_libraries
+WHERE visits >= 0;
+
+SELECT sum(visits) AS visits_2016
+FROM pls_fy2016_libraries
+WHERE visits >= 0;
+
+
+
+
+
+
 
