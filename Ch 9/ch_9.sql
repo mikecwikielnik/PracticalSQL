9d08e806c0bda4486ae44582067bc3dd5c201913
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-04-23 18:35:06 -0400

    Add files via upload

--- Ch 9/ch_9.sql
+++ Ch 9/ch_9.sql
@@ -303,10 +303,27 @@ WHERE pls18.visits >= 0
 GROUP BY pls18.stabr
 ORDER BY chg_2018_17 DESC;
 
+-- Listing 9-14: Using a HAVING clause to filter the results of an aggregate query
 
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 4976-4977). Kindle Edition. 
 
-
-
+SELECT pls18.stabr,
+	sum(pls18.visits) AS visits_2018,
+	sum(pls17.visits) AS visits_2017,
+	sum(pls16.visits) AS visits_2016,
+	round((sum(pls18.visits::numeric) - sum(pls17.visits)) / 
+		 sum(pls17.visits)*100, 1) AS chg_2018_17,
+	round((sum(pls17.visits::numeric) - sum(pls16.visits)) / 
+		 sum(pls16.visits)*100, 1) AS chg_2017_16
+FROM pls_fy2018_libraries pls18
+	JOIN pls_fy2017_libraries pls17 ON pls18.fscskey = pls17.fscskey
+	JOIN pls_fy2016_libraries pls16 ON pls18.fscskey = pls16.fscskey
+WHERE pls18.visits >= 0
+	AND pls17.visits >= 0
+	AND pls16.visits >= 0
+GROUP BY pls18.stabr
+HAVING sum(pls18.visits) > 50000000
+ORDER BY chg_2018_17 DESC;
 
 
 
