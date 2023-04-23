2448f69dff01c68a47b0dbda035e3037746f943b
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-04-23 17:16:21 -0400

    Add files via upload

--- Ch 9/ch_9.sql
+++ Ch 9/ch_9.sql
@@ -267,9 +267,20 @@ SELECT sum(visits) AS visits_2016
 FROM pls_fy2016_libraries
 WHERE visits >= 0;
 
+-- Listing 9-12: Using sum() to total visits on joined 2018, 2017, and 2016 tables
 
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 4876-4877). Kindle Edition. 
 
-
+SELECT 
+	sum(pls18.visits) AS visits_2018,
+	sum(pls17.visits) AS visits_2017,
+	sum(pls16.visits) AS visits_2016
+FROM pls_fy2018_libraries pls18
+	JOIN pls_fy2017_libraries pls17 ON pls18.fscskey = pls17.fscskey
+	JOIN pls_fy2016_libraries pls16 ON pls18.fscskey = pls16.fscskey
+WHERE pls18.visits >= 0
+	AND pls17.visits >= 0
+	AND pls16.visits >= 0;
 
 
 
