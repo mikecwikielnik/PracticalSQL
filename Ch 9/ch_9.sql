3dbf6901d365cfa44c2b5db5a2676f32f4c00583
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-04-22 20:12:34 -0400

    Add files via upload

---
+++ Ch 9/ch_9.sql
@@ -0,0 +1,65 @@
+-- Chapter 9
+-- Extracting Information by Grouping and Summarizing
+
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 4579-4580). Kindle Edition.
+
+-- Listing 9-1: Creating and filling the 2018 Public Libraries Survey table
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 4625). Kindle Edition. 
+
+CREATE TABLE pls_fy2018_libraries (
+    stabr text NOT NULL,
+    fscskey text CONSTRAINT fscskey_2018_pkey PRIMARY KEY,
+    libid text NOT NULL,
+    libname text NOT NULL,
+    address text NOT NULL,
+    city text NOT NULL,
+    zip text NOT NULL,
+    county text NOT NULL,
+    phone text NOT NULL,
+    c_relatn text NOT NULL,
+    c_legbas text NOT NULL,
+    c_admin text NOT NULL,
+    c_fscs text NOT NULL,
+    geocode text NOT NULL,
+    lsabound text NOT NULL,
+    startdate text NOT NULL,
+    enddate text NOT NULL,
+    popu_lsa integer NOT NULL,
+    popu_und integer NOT NULL,
+    centlib integer NOT NULL,
+    branlib integer NOT NULL,
+    bkmob integer NOT NULL,
+    totstaff numeric(8,2) NOT NULL,
+    bkvol integer NOT NULL,
+    ebook integer NOT NULL,
+    audio_ph integer NOT NULL,
+    audio_dl integer NOT NULL,
+    video_ph integer NOT NULL,
+    video_dl integer NOT NULL,
+    ec_lo_ot integer NOT NULL,
+    subscrip integer NOT NULL,
+    hrs_open integer NOT NULL,
+    visits integer NOT NULL,
+    reference integer NOT NULL,
+    regbor integer NOT NULL,
+    totcir integer NOT NULL,
+    kidcircl integer NOT NULL,
+    totpro integer NOT NULL,
+    gpterms integer NOT NULL,
+    pitusr integer NOT NULL,
+    wifisess integer NOT NULL,
+    obereg text NOT NULL,
+    statstru text NOT NULL,
+    statname text NOT NULL,
+    stataddr text NOT NULL,
+    longitude numeric(10,7) NOT NULL,
+    latitude numeric(10,7) NOT NULL
+);
+
+COPY pls_fy2018_libraries
+FROM 'C:\Users\mikec\OneDrive\Google One Drive\Google Drive\SQL\Practical SQL, 2nd Ed\practical-sql-2-main\Chapter_09\pls_fy2018_libraries.csv'
+WITH (FORMAT CSV, HEADER);
+
+CREATE INDEX libname_2018_idx ON pls_fy2018_libraries (libname);
+
