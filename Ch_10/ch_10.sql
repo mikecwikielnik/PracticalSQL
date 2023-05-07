74117b05a1dbc1ad2df44304fc881e2ef52e08bf
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-06 21:20:35 -0400

    Add files via upload

--- Ch_10/ch_10.sql
+++ Ch_10/ch_10.sql
@@ -219,5 +219,18 @@ COPY state_regions
 FROM 'C:\Users\mikec\OneDrive\Google One Drive\Google Drive\SQL\Practical SQL, 2nd Ed\practical-sql-2-main\Chapter_10\state_regions.csv'
 WITH (FORMAT CSV, HEADER);
 
+-- Listing 10-19: Adding and updating an inspection_deadline column
 
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 5488-5489). Kindle Edition. 
 
+ALTER TABLE meat_poultry_egg_establishments
+ADD COLUMN inspection_deadline timestamp with time zone;
+
+UPDATE meat_poultry_egg_establishments
+SET inspectioin_deadline = '2022-12-01 00:00 EST'
+WHERE EXISTS (SELECT state_regions.region
+			  FROM state regions
+			  WHERE establishments.st = state_regions.st
+			  AND state_regions = 'New England');
+			  
+			  
