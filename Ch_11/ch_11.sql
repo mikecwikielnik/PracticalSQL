cf72bfe485c512e4815c714358598cf3fa851f9a
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-10 16:29:34 -0400

    Add files via upload

--- Ch_11/ch_11.sql
+++ Ch_11/ch_11.sql
@@ -159,8 +159,23 @@ FROM cbp_naics_72_establishments
 ORDER BY state_fips, county_fips
 LIMIT 5;
 
+-- Listing 11-9: Finding business rates per thousand population in counties with 50,000 or more people
 
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 6066-6067). Kindle Edition. 
 
+SELECT 
+	cbp.county, 
+	cbp.st,
+	cbp.establishments,
+	pop.pop_est_2018,
+	round((cbp.establishments::numeric / pop.pop_est_2018) * 1000, 1)
+		as estabs_per_1000
+FROM cbp_naics_72_establishments cbp 
+JOIN us_counties_pop_est_2019 pop
+ON cbp.state_fips = pop.state_fips
+AND cbp.county_fips = pop.county_fips
+WHERE pop.pop_est_2018 >= 50000
+ORDER BY cbp.establishments::numeric / pop.pop_est_2018 DESC;
 
 
 
