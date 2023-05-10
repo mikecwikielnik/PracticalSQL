fc6297799d55e1164f3fed0eac5f0b7b8b3a6f1c
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-10 16:20:50 -0400

    Add files via upload

--- Ch_11/ch_11.sql
+++ Ch_11/ch_11.sql
@@ -132,15 +132,32 @@ SELECT
 	unit_sales,
 	rank() OVER (PARTITION BY category ORDER BY unit_sales DESC)
 FROM store_sales
-ORDER BY category, rank() OVER (PARTITION BY category
-							   ORDER BY unit_sales DESC);
+ORDER BY category, rank() OVER (PARTITION BY category ORDER BY unit_sales DESC);
 
+-- Listing 11-8: Creating and filling a table for census county business pattern data
 
+-- Anthony DeBarros. 9781718501072 (Kindle Location 6053). Kindle Edition. 
 
+CREATE TABLE cbp_naics_72_establishments (
+	state_fips text,
+	county_fips text,
+	county text NOT NULL,
+	st text NOT NULL,
+	naics_2017 text NOT NULL,
+	naics_2017_label text NOT NULL,
+	year smallint NOT NULL,
+	establishments integer NOT NULL,
+	CONSTRAINT cbp_fips_key PRIMARY KEY (state_fips, county_fips)
+);
 
+COPY cbp_naics_72_establishments
+FROM 'C:\Users\mikec\OneDrive\Google One Drive\Google Drive\SQL\Practical SQL, 2nd Ed\practical-sql-2-main\Chapter_11\cbp_naics_72_establishments.csv'
+WITH (FORMAT CSV, HEADER);
 
-
-
+SELECT *
+FROM cbp_naics_72_establishments
+ORDER BY state_fips, county_fips
+LIMIT 5;
 
 
 
