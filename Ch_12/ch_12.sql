f74265a21cd0bbb23e4cee4384a5ea7b8e8a3657
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-13 13:18:17 -0400

    Add files via upload

--- Ch_12/ch_12.sql
+++ Ch_12/ch_12.sql
@@ -20,9 +20,34 @@ SELECT make_date(2022, 2, 22);
 SELECT make_time(18, 4, 30.3);
 SELECT make_timestamptz(2022, 2, 22, 18, 4, 30.3, 'Europe/Lisbon');
 
+-- Bonus: Retrieving the current date and time
+
+SELECT
+    current_timestamp,
+    localtimestamp,
+    current_date,
+    current_time,
+    localtime,
+    now();
 
 
+-- Listing 12-3: Comparing current_timestamp and clock_timestamp() during row insert
 
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 6347-6348). Kindle Edition. 
+
+CREATE TABLE current_time_example (
+	time_id integer GENERATED ALWAYS AS IDENTITY,
+	current_timestamp_col timestamptz,
+	clock_timestamp_col timestamptz
+);
+
+INSERT INTO current_time_example
+		(current_timestamp_col, clock_timestamp_col)
+	(SELECT current_timestamp,
+		clock_timestamp()
+	FROM generate_series(1, 1000));
+
+SELECT * FROM current_time_example;
 
 
 
