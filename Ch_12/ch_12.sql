18fb81f567c437a003d9b746d2ef8f15e449c7f9
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-13 12:37:26 -0400

    Add files via upload

--- Ch_12/ch_12.sql
+++ Ch_12/ch_12.sql
@@ -12,6 +12,21 @@ SELECT
 	date_part('month', '2022-12-01 18:37:12 EST'::timestamptz) AS month,
 	date_part('epoch', '2022-12-01 18:37:12 EST'::timestamptz) AS epoch;
 
+-- Listing 12-2: Three functions for making datetimes from components
+
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 6313-6314). Kindle Edition. 
+
+SELECT make_date(2022, 2, 22);
+SELECT make_time(18, 4, 30.3);
+SELECT make_timestamptz(2022, 2, 22, 18, 4, 30.3, 'Europe/Lisbon');
+
+
+
+
+
+
+
+
 
 
 
