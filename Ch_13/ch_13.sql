2200d5af74b1373267f918484f6b3332e751fa11
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-15 14:02:27 -0400

    Add files via upload

--- Ch_13/ch_13.sql
+++ Ch_13/ch_13.sql
@@ -17,9 +17,21 @@ WHERE pop_est_2019 >= (
 )
 ORDER BY pop_est_2019 DESC;
 
+-- Listing 13-2: Using a subquery in a WHERE clause with DELETE
 
+-- Anthony DeBarros. 9781718501072 (Kindle Location 6781). Kindle Edition. 
 
+CREATE TABLE us_counties_2019_top10 AS
 
+SELECT * FROM us_counties_pop_est_2019;
+
+DELETE FROM us_counties_2019_top10
+WHERE pop_est_2019 < (
+	SELECT percentile_cont(.9) WITHIN GROUP (ORDER BY  pop_est_2019)
+	FROM us_counties_2019_top10
+);
+
+select * from us_counties_2019_top10;
 
 
 
