8a9968169607e33ad51acc49658f91144ea79cd0
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-15 11:57:12 -0400

    Add files via upload

--- Ch_12/ch_12.sql
+++ Ch_12/ch_12.sql
@@ -181,6 +181,39 @@ FROM nyc_yellow_taxi_trips
 GROUP BY trip_hour
 ORDER BY trip_hour;
 
+-- Listing 12-11: Creating a table to hold train trip data
+
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 6605-6606). Kindle Edition. 
+
+CREATE TABLE train_rides (
+	trip_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	segment text NOT NULL,
+	departure timestamptz NOT NULL,
+	arrival timestamptz NOT NULL
+);
+
+INSERT INTO train_rides (segment, departure, arrival)
+VALUES
+	('Chicago to New York', '2020-11-13 21:30 CST', '2020-11-14 18:23 EST'),
+	('New York to New Orleans', '2020-11-15 14:15 EST', '2020-11-16 19:32 CST'),
+	('New Orleans to Los Angeles', '2020-11-17 13:45 CST', '2020-11-18 9:00 PST'),
+	('Los Angeles to San Francisco', '2020-11-20 9:10 PST', '2020-11-19 21:24 PST'),
+	('San Francisco to Denver', '2020-11-20 9:10 PST', '2020-11-21 18:38 MST'),
+	('Denver to Chicago', '2020-11-22 19:10 MST', '2020-11-23 14:50 CST');
+	
+SET TIME ZONE 'US/Central';
+
+SELECT * FROM train_rides;
+
+
+
+
+
+
+
+
+
+
 
 
 
