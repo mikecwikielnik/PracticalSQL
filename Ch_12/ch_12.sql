1b5c1be68302847f84dbac9fb1f560e40357582f
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-15 10:45:59 -0400

    Add files via upload

--- Ch_12/ch_12.sql
+++ Ch_12/ch_12.sql
@@ -86,6 +86,74 @@ FROM time_Zone_test;
 SELECT test_date AT TIME ZONE 'Asia/Seoul'
 FROM time_zone_test;
 
+-- Listing 12-7: Creating a table and importing NYC yellow taxi data
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 6494). Kindle Edition. 
+
+CREATE TABLE nyc_yellow_taxi_trips (
+	trip_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	vendor_id text NOT NULL,
+	tpep_pickup_datetime timestamptz NOT NULL,
+	tpep_dropoff_datetime timestamptz NOT NULL,
+	passenger_count integer NOT NULL,
+	trip_distance numerIC(8, 2) NOT NULL,
+	pickup_longitude numeric(18, 15) NOT NULL,
+	pickup_latitude numeric(18, 15) NOT NULL,
+	rate_code_id text NOT NULL,
+	store_and_fwd_flag text NOT NULL,
+	dropoff_longitude numeric(18, 15) NOT NULL,
+	dropoff_latitude numeric(18, 15) NOT NULL,
+	payment_type text NOT NULL,
+	fare_amount numeric(9, 2) NOT NULL,
+	extra numeric(9, 2) NOT NULL,
+	mta_tax numeric(5, 2) NOT NULL,
+	tip_amount numeric(9, 2) NOT NULL,
+	tolls_amount numeric(9, 2) NOT NULL,
+	improvement_surcharge numeric(9, 2) NOT NULL,
+	total_amount numeric(9, 2) NOT NULL
+);
+
+COPY nyc_yellow_taxi_trips (
+	vendor_id,
+	tpep_pickup_datetime,
+	tpep_dropoff_datetime, 
+	passenger_count,
+	trip_distance,
+	pickup_longitude,
+	pickup_latitude,
+	rate_code_id,
+	store_and_fwd_flag,
+	dropoff_longitude,
+	dropoff_latitude,
+	payment_type,
+	fare_amount,
+	extra,
+	mta_tax,
+	tip_amount,
+	tolls_amount,
+	improvement_surcharge,
+	total_amount
+)
+FROM 'C:\Users\mikec\OneDrive\Google One Drive\Google Drive\SQL\Practical SQL, 2nd Ed\practical-sql-2-main\Chapter_12\nyc_yellow_taxi_trips.csv'
+WITH (FORMAT CSV, HEADER);
+
+CREATE INDEX tpep_pickup_idx
+ON nyc_yellow_taxi_trips (tpep_pickup_datetime);
+
+select * from nyc_yellow_taxi_trips;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
