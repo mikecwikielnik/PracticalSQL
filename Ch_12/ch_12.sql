74e0b4cec607112ab0dd2ce07c0dd157ece829eb
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-15 11:43:43 -0400

    Add files via upload

--- Ch_12/ch_12.sql
+++ Ch_12/ch_12.sql
@@ -168,9 +168,18 @@ COPY
 TO 'C:\Users\mikec\OneDrive\Google One Drive\Google Drive\SQL\Practical SQL, 2nd Ed\practical-sql-2-main\Chapter_12\hourly_taxi_pickups.csv'
 WITH (FORMAT CSV, HEADER);
 
+-- Listing 12-10: Calculating median trip time by hour
 
+-- Anthony DeBarros. 9781718501072 (Kindle Location 6569). Kindle Edition. 
 
-
+SELECT
+	date_part('hour', tpep_pickup_datetime) AS trip_hour,
+	percentile_cont(.5)
+	WITHIN GROUP (ORDER BY
+				 tpep_dropoff_datetime - tpep_pickup_datetime) AS median_trip
+FROM nyc_yellow_taxi_trips
+GROUP BY trip_hour
+ORDER BY trip_hour;
 
 
 
