71ed27f0216f7e25bc85d52699b5ad17d26b8f1c
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-15 15:43:29 -0400

    Add files via upload

--- Ch_13/ch_13.sql
+++ Ch_13/ch_13.sql
@@ -55,6 +55,41 @@ AS calcs;
 select pop_est_2019
 from us_counties_pop_est_2019;
 
+-- Listing 13-4: Joining two derived tables
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 6828). Kindle Edition. 
+
+SELECT census.state_name AS st,
+	census.pop_est_2018,
+	est.establishment_count,
+	round((est.establishment_count/census.pop_est_2018::numeric) * 1000, 1)
+	AS estabs_per_thousand
+FROM (
+	SELECT st,
+		sum(establishments) AS establishment_count
+	FROM cbp_naics_72_establishments
+	GROUP BY st
+	) AS est
+JOIN (
+	SELECT state_name, 
+		sum(pop_est_2018) AS pop_est_2018
+	FROM us_counties_pop_est_2019
+	GROUP BY state_name
+	) AS census
+ON est.st = census.state_name
+ORDER BY estabs_per_thousand DESC;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
