2df7748400024b21c9955160a6ab175c87e79830
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-15 21:53:02 -0400

    Add files via upload

--- Ch_13/ch_13.sql
+++ Ch_13/ch_13.sql
@@ -154,12 +154,52 @@ WHERE NOT EXISTS (
 	FROM retirees
 	WHERE id = employees.emp_id);
 
+-- Listing 13-11: Using LATERAL subqueries in the FROM clause
 
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 6974-6975). Kindle Edition. 
 
+SELECT county_name,
+	state_name,
+	pop_est_2018,
+	pop_est_2019,
+	raw_chg,
+	round(pct_chg * 100, 2) AS pct_chg
+FROM us_counties_pop_est_2019,
+	LATERAL (SELECT pop_est_2019 - pop_est_2018 AS raw_chg) rc,
+	LATERAL (SELECT raw_chg/pop_est_2018::numeric AS pct_chg) pc
+ORDER BY pct_chg DESC;
 
+-- Listing 13-12: Using a subquery with a LATERAL join
 
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 7010-7011). Kindle Edition. 
 
+ALTER TABLE teachers ADD CONSTRAINT id_key PRIMARY KEY (id);
 
+CREATE TABLE teachers_lab_access (
+	access_id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	access_time timestamp with time zone,
+	lab_name text,
+	teacher_id bigint REFERENCES teachers (id)
+);
+
+INSERT INTO teachers_lab_access (access_time, lab_name, teacher_id)
+VALUES 
+	('2022-11-30 08:59:00-05', 'Science A', 2),
+	('2022-12-01 08:58:00-05', 'Chemistry B', 2),
+	('2022-12-21 09:01:00-05', 'Chemistry A', 2),
+	('2022-12-02 11:01:00-05', 'Science B', 6),
+	('2022-12-07 10:02:00-05', 'Science A', 6),
+	('2022-12-17 16:00:00-05', 'Science B', 6);
+
+SELECT t.first_name, t.last_name, a.access_time, a.lab_name
+FROM teachers t
+LEFT JOIN LATERAL (SELECT * 
+				   FROM teachers_lab_access
+				   WHERE teacher_id = t.id
+				   ORDER BY access_time DESC
+				   LIMIT 2) A
+ON true
+ORDER by t.id;
 
 
 
