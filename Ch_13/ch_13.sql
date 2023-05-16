be62a225f5998c6caf3c2de16b4b22ec0a1b0d0e
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-16 09:57:00 -0400

    Add files via upload

--- Ch_13/ch_13.sql
+++ Ch_13/ch_13.sql
@@ -201,6 +201,29 @@ LEFT JOIN LATERAL (SELECT *
 ON true
 ORDER by t.id;
 
+-- Listing 13-13: Using a simple CTE to count large counties
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 7047). Kindle Edition. 
+
+WITH large_counties (county_name, state_name, pop_est_2019)
+AS (
+	SELECT county_name, state_name, pop_est_2019
+	FROM us_counties_pop_est_2019
+	WHERE pop_est_2019 >= 100000
+)
+SELECT state_name, count(*)
+FROM large_counties
+GROUP BY state_name
+ORDER BY count(*) DESC;
+
+
+
+
+
+
+
+
+
 
 
 
