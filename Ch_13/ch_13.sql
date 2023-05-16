f96f941a8c9131b04c4dd462283c2dc08661b46a
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-16 10:41:05 -0400

    Add files via upload

--- Ch_13/ch_13.sql
+++ Ch_13/ch_13.sql
@@ -241,11 +241,23 @@ FROM counties JOIN establishments
 ON counties.st = establishments.st
 ORDER BY estabs_per_thousand DESC;
 
+-- Listing 13-15: Using CTEs to minimize redundant code
 
+-- Anthony DeBarros. 9781718501072 (Kindle Location 7090). Kindle Edition. 
 
+WITH us_median AS
+	(SELECT  percentile_cont(.5)
+	WITHIN GROUP (ORDER BY pop_est_2019) AS us_median_pop
+	FROM us_counties_pop_est_2019) 
 
-
-
+SELECT county_name,
+	state_name AS st,
+	pop_est_2019,
+	us_median_pop,
+	pop_est_2019 - us_median_pop AS diff_from_median
+FROM us_counties_pop_est_2019 CROSS JOIN us_median
+WHERE (pop_est_2019 - us_median_pop)
+BETWEEN -1000 AND 1000;
 
 
 
