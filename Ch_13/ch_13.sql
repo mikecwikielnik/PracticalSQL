eca773f49731f1f2dfa4174a229774f1c34e80b7
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-16 15:27:16 -0400

    Add files via upload

--- Ch_13/ch_13.sql
+++ Ch_13/ch_13.sql
@@ -362,5 +362,36 @@ SELECT max_temp,
 FROM temperature_readings
 ORDER BY station_name, observation_date;
 
+-- Listing 13-21: Using CASE in a CTE
+
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 7294-7295). Kindle Edition. 
+
+WITH temps_collapsed (station_name, max_temperature_group) AS
+	(SELECT station_name,
+		CASE WHEN max_temp >= 90 THEN 'hot'
+			WHEN max_temp >= 70 AND max_temp < 90 THEN 'Warm'
+			WHEN max_temp >= 50 AND max_temp < 70 THEN 'Pleasant'
+			WHEN max_temp >= 33 AND max_temp < 50 THEN 'Cold'
+			WHEN max_temp >= 20 AND max_temp < 33 THEN 'Frigid'
+			WHEN max_temp < 20 THEN 'Inhumane'
+			ELSE 'No reading'
+		END
+	FROM temperature_readings)
+	
+SELECT station_name, max_temperature_group, count(*)
+FROM temps_collapsed
+GROUP BY station_name, max_temperature_group
+ORDER BY station_name, count(*) DESC;
+
+
+
+
+
+
+
+
+
+
+
 
 
