e2ff87c021b831f6f176b5bf6687e8975b0dd144
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-16 13:20:37 -0400

    Add files via upload

--- Ch_13/ch_13.sql
+++ Ch_13/ch_13.sql
@@ -275,6 +275,27 @@ COPY ice_cream_survey
 FROM 'C:\Users\mikec\OneDrive\Google One Drive\Google Drive\SQL\Practical SQL, 2nd Ed\practical-sql-2-main\Chapter_13\ice_cream_survey.csv'
 WITH (FORMAT CSV, HEADER);
 
+-- Listing 13-17: Generating the ice cream survey crosstab
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 7162). Kindle Edition. 
+
+SELECT *
+FROM crosstab('SELECT office,
+			  	flavor,
+			  	count(*)
+			  FROM ice_cream_survey
+			  GROUP BY office, flavor
+			  ORDER BY office',
+			 
+			 'SELECT flavor
+			  FROM ice_cream_survey
+			  GROUP BY flavor
+			  ORDER BY flavor')
+AS (office text,
+   chocolate bigint,
+   strawberry bigint,
+   vanilla bigint);
+
 
 
 
