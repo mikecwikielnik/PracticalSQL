8e0a03180ff67e314f5ae209461a511a1f2fba6f
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-18 19:48:02 -0400

    Add files via upload

--- Ch_14/ch_14.sql
+++ Ch_14/ch_14.sql
@@ -17,6 +17,24 @@ FROM us_counties_pop_est_2019
 WHERE county_name ~* 'ash' AND county_name !~ 'Wash'
 ORDER BY county_name;
 
+-- Listing 14-2: Regular expression functions to replace and split text
+
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 7531-7532). Kindle Edition. 
+
+SELECT regexp_replace('05/12/2024', '\d{4}', '2023');
+
+SELECT regexp_split_to_table('Four,score,and,seven,years,ago',','); -- notice the name of the fn's
+
+SELECT regexp_split_to_array('Phil Mike Tony Steve', ''); -- again, notice the name of the fn's
+
+-- regexp_replace(string, pattern, replacement text)
+-- regexp_split_to_table(string, pattern)
+-- regexp_split_to_array(string, pattern)
+
+
+
+
+
 
 
 
