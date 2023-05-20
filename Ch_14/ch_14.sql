3b59361d0938395ae0d129d7af871477597f24cb
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-20 19:34:05 -0400

    Add files via upload

--- Ch_14/ch_14.sql
+++ Ch_14/ch_14.sql
@@ -133,11 +133,45 @@ SET date_1 =
 	)::timestamptz
 RETURNING crime_id, date_1, original_text;
 
+-- Listing 14-13: Updating all crime_reports columns
 
+-- Anthony DeBarros. 9781718501072 (Kindle Location 7861). Kindle Edition. 
 
-
-
-
+UPDATE crime_reports
+SET date_1 = 
+	(
+	(regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}'))[1]
+		||''||
+	(regexp_match(original_text, '\/\d{2}\n(\d{4})'))[1]
+		||'US/Eastern'
+	)::timestamptz,
+	
+	date_2 =
+	CASE
+		WHEN (SELECT regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{2})')IS NULL)
+			AND (SELECT regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})')IS NOT NULL)
+		THEN 
+		((regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}'))[1]
+		 ||''||
+		(regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})'))[1]
+		 ||'US/Eastern'
+		)::timestamptz
+		
+		WHEN (SELECT regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{2})')IS NOT NULL)
+			AND (SELECT regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})')IS NOT NULL)
+		THEN
+		((regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{1,2})'))[1]
+		 ||''||
+		(regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})'))[1]
+		 ||'US/Eastern'
+		)::timestamptz
+	END,
+	street = (regexp_match(original_text, 'hrs.\n(\d+ .+(?:Sq.|Plz.|Dr.|Ter.|Rd.))'))[1],
+	city = (regexp_match(original_text,
+						'(?:Sq.|Plz.|Dr.|Ter.|Rd.)\n(\w+\w+|\w+)\n'))[1],
+	crime_type = (regexp_match(original_text, '\n(?:\w+\w+|\w+)\n(.*):'))[1],
+	description = (regexp_match(original_text, ':\s(.+)(?:C0|SO)'))[1],
+	case_number = (regexp_match(original_text, '(?:C0|SO)[0-9]+'))[1];
 
 
 
