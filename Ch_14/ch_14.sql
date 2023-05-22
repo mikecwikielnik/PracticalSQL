9652d9623012f4b0ee416a89cea37c91b65f6d6c
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-22 15:44:16 -0400

    Add files via upload

--- Ch_14/ch_14.sql
+++ Ch_14/ch_14.sql
@@ -313,8 +313,19 @@ WHERE search_speech_text @@
 ORDER BY score DESC
 LIMIT 5;
 
+-- Listing 14-26: Normalizing ts_rank() by speech length
 
+-- Anthony DeBarros. 9781718501072 (Kindle Location 8152). Kindle Edition. 
 
+SELECT president,
+	speech_date,
+	ts_rank(search_speech_text,
+		   to_tsquery('english', 'war & security & threat & enemy'), 2):: numeric AS score
+FROM president_speeches
+WHERE search_speech_text @@
+	to_tsquery('english', 'war & security & threat & enemy')
+ORDER BY score DESC
+LIMIT 5;
 
 
 
