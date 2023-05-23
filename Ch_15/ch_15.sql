e5a2d1b5098c78cfaed1c6754a85b50df725c53f
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-23 17:27:24 -0400

    Add files via upload

--- Ch_15/ch_15.sql
+++ Ch_15/ch_15.sql
@@ -146,12 +146,22 @@ SELECT ST_Distance(
 	ST_GeogFromText('POINT(-73.8480153 40.7570917)')
 	)/1609.344 AS mets_to_yanks;
 
+-- Listing 15-12: Using ST_Distance() for each row in farmers_markets
 
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 8587-8588). Kindle Edition. 
 
-
-
-
-
+SELECT market_name,
+	city,
+	round(
+		(ST_Distance(geog_point,
+					ST_GeogFromText('POINT(-93.6204386 41.5853202)')
+					)/1609.344)::numeric, 2
+		) AS miles_from_dt
+FROM farmers_markets
+WHERE ST_DWithin(geog_point,
+				ST_GeogFromText('POINT(-93.6204386 41.5853202)'),
+				10000)
+ORDER BY miles_from_dt ASC;				
 
 
 
