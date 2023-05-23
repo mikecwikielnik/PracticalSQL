9291e08e5a071fc5a11e1c9e6ba4915c5457d7e7
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-23 15:22:15 -0400

    Add files via upload

---
+++ Ch_15/ch_15.sql
@@ -0,0 +1,13 @@
+-- Chapter 15 
+-- Analyzing Spatial Data with PostGIS
+
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 8185-8186). Kindle Edition. 
+
+-- Listing 15-1: Loading the PostGIS extension
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 8211). Kindle Edition. 
+
+CREATE EXTENSION postgis;
+
+SELECT postgis_full_version();
+
