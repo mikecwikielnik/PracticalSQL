b37bbe1e3afb266beca4331be19f6496171d7d3a
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-23 16:24:54 -0400

    Add files via upload

--- Ch_15/ch_15.sql
+++ Ch_15/ch_15.sql
@@ -42,10 +42,12 @@ SELECT ST_GeomFromText('MULTIPOLYGON((
 					  -74.98 42.64, -74.98 42.66,
 					  -75.0 42.66)))', 4326);
 
+-- Listing 15-4: Using ST_GeogFromText() to create spatial objects
 
+-- Anthony DeBarros. 9781718501072 (Kindle Location 8385). Kindle Edition. 
 
-
-
+SELECT
+ST_GeogFromText('SRID = 4326; MULTIPOINT(-74.9 42.7, -75.1 42.7, -74.924 42.6)')
 
 
 
