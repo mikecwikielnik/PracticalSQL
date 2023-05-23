8b260308400f613e47381e05a8a56fd6869a511b
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-23 16:10:40 -0400

    Add files via upload

--- Ch_15/ch_15.sql
+++ Ch_15/ch_15.sql
@@ -19,9 +19,28 @@ SELECT srtext
 FROM spatial_ref_sys
 WHERE srid = 4326;
 
+-- Listing 15-3: Using ST_GeomFromText() to create spatial objects
 
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 8357-8358). Kindle Edition. 
 
+SELECT ST_GeomFromText('POINT(-74.9233606 42.699992)', 4326);
 
+SELECT ST_GeomFromText('LINESTRING(-74.49 42.7, -75.1 42.7)', 4326);
+
+SELECT ST_GeomFromText('POLYGON((-74.9 42.7, -75.1 42.7, -75.1 42.6, -74.9 42.7))', 4362);
+
+SELECT ST_GeomFromText('MULTIPOINT(-74.9 42.7, -75.1 42.7)', 4326);
+
+SELECT ST_GeomFromText('MULTILINESTRING((-76.27 43.1, -76.06 43.08),
+					  (-76.2 43.3, -76.2 43.4,
+					  -76.4 43.1))', 4326);
+					  
+SELECT ST_GeomFromText('MULTIPOLYGON((
+					  (-74.92 42.7, -75.06 42.71,
+					  -75.07 42.64, -74.92 42.7),
+					  (-75.0 42.66, -75.0 42.64,
+					  -74.98 42.64, -74.98 42.66,
+					  -75.0 42.66)))', 4326);
 
 
 
