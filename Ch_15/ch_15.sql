e835e51ebb25fc069d5f9f6008bea5268ff0605a
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-23 17:02:19 -0400

    Add files via upload

--- Ch_15/ch_15.sql
+++ Ch_15/ch_15.sql
@@ -102,9 +102,26 @@ COPY farmers_markets
 FROM 'C:\Users\mikec\OneDrive\Google One Drive\Google Drive\SQL\Practical SQL, 2nd Ed\practical-sql-2-main\Chapter_15\farmers_markets.csv'
 WITH (FORMAT CSV, HEADER);
 
+-- Listing 15-9: Creating and indexing a geography column
 
+-- Anthony DeBarros. 9781718501072 (Kindle Location 8483). Kindle Edition. 
 
+ALTER TABLE farmers_markets ADD COLUMN geog_point geography(POINT, 4326);
 
+UPDATE farmers_markets
+SET geog_point =
+	ST_SetSRID(
+		ST_MakePoint(longitude, latitude)::geography, 4326);
+		
+CREATE INDEX market_pts_idx ON farmers_markets USING GIST (geog_point);
+
+SELECT longitude,
+	latitude,
+	geog_point,
+	ST_AsEWKT(geog_point)
+FROM farmers_markets
+WHERE longitude IS NOT NULL
+LIMIT 5;
 
 
 
