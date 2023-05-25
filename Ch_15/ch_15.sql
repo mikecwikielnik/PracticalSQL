fe0b7f6cf3eed13973d1100987016d06d89c0154
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-25 15:15:10 -0400

    Add files via upload

--- Ch_15/ch_15.sql
+++ Ch_15/ch_15.sql
@@ -213,9 +213,16 @@ WHERE ST_Within(
 	'SRID=4269; POINT(-118.3419063 34.0977076)'::geometry, geom
 )
 
+-- Listing 15-17: Using ST_DWithin() to count people near Lincoln, Nebraska
 
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 8824-8825). Kindle Edition. 
 
-
+SELECT sum(c.pop_est_2019) AS pop_est_2019
+FROM us_Counties_2019_shp sh JOIN us_counties_pop_est_2019 c
+	ON sh.statefp = c.state_fips AND sh.countyfp = c.county_fips
+WHERE ST_DWithin(sh.geom::geography,
+				ST_GeogFromText('SRID=4269;POINT(-96.699656 40.811567)'),
+				80467);
 
 
 
