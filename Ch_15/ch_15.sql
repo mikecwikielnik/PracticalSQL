93fceba44581545481aa930b2a1d8a437224dcd3
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-25 15:54:34 -0400

    Add files via upload

--- Ch_15/ch_15.sql
+++ Ch_15/ch_15.sql
@@ -250,4 +250,26 @@ SELECT ST_GeometryType(geom)
 FROM santafe_roads_2019
 LIMIT 1;
 
+-- Listing 15-20: Spatial join with ST_Intersects() to find roads crossing the Santa Fe River
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 8884). Kindle Edition. 
+
+SELECT water.fullname AS waterway,
+	roads.rttyp,
+	roads.fullname AS road
+FROM santafe_linearwater_2019 water JOIN santafe_roads_2019 roads
+	ON ST_Intersects(water.geom, roads.geom)
+WHERE water.fullname = 'Santa Fe Riv'
+	AND roads.fullname IS NOT NULL
+ORDER BY roads.fullname;	
+
+
+
+
+
+
+
+
+
+
 
