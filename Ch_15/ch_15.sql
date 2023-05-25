cb32ee2e31e858446b82997b325e156795aad038
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-25 15:22:47 -0400

    Add files via upload

--- Ch_15/ch_15.sql
+++ Ch_15/ch_15.sql
@@ -224,9 +224,19 @@ WHERE ST_DWithin(sh.geom::geography,
 				ST_GeogFromText('SRID=4269;POINT(-96.699656 40.811567)'),
 				80467);
 
+-- Listing 15-18: Displaying counties near Lincoln, Nebraska
 
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 8837-8838). Kindle Edition. 
 
-
+SELECT sh.name,
+	c.state_name,
+	c.pop_est_2019,
+	ST_Transform(sh.geom, 4326) AS geom
+FROM us_counties_2019_shp sh JOIN us_counties_pop_est_2019 c
+	ON sh.statefp = c.state_fips AND sh.countyfp = c.county_fips
+WHERE ST_DWithin(geom::geography,
+				ST_GeogFromText('SRID=4269;POINT(-96.699656 40.811567)'),
+				80467);	
 
 
 
