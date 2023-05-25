29bb7888e44d652a4b3a4117e139bff074696303
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-25 15:08:09 -0400

    Add files via upload

--- Ch_15/ch_15.sql
+++ Ch_15/ch_15.sql
@@ -201,9 +201,17 @@ FROM us_counties_2019_shp
 ORDER BY square_miles DESC
 LIMIT 5;
 
+-- Listing 15-16: Using ST_Within() to find the county belonging to a pair of coordinates
 
+-- Anthony DeBarros. 9781718501072 (Kindle Location 8798). Kindle Edition. 
 
-
+SELECT sh.name,
+	c.state_name
+FROM us_counties_2019_shp sh JOIN us_counties_pop_est_2019 c
+ON sh.statefp = c.state_fips AND sh.countyfp = c.county_fips
+WHERE ST_Within(
+	'SRID=4269; POINT(-118.3419063 34.0977076)'::geometry, geom
+)
 
 
 
