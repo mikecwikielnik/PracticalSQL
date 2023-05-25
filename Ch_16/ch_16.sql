9efb7940579352cdff09590d7ba3bd7b8939898e
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-25 16:19:05 -0400

    Add files via upload

---
+++ Ch_16/ch_16.sql
@@ -0,0 +1,36 @@
+-- Chapter 16 
+-- Working with JSON Data
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 8953). Kindle Edition. 
+
+-- Listing 16-2: Creating a table to hold JSON data and adding an index
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 9068). Kindle Edition. 
+
+CREATE TABLE films(
+	id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	film jsonb NOT NULL
+);
+
+COPY films(film)
+FROM 'C:\Users\mikec\OneDrive\Google One Drive\Google Drive\SQL\Practical SQL, 2nd Ed\practical-sql-2-main\Chapter_16\films.json';
+
+CREATE INDEX idx_film ON films USING GIN (film);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
