6959f9af94750f1ffb47cf0a2d7b0decc48b7293
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-29 14:46:04 -0400

    Add files via upload

--- Ch_16/ch_16.sql
+++ Ch_16/ch_16.sql
@@ -175,9 +175,18 @@ FROM earthquakes
 ORDER BY (earthquake #>> '{properties, mag}')::numeric DESC NULLS LAST
 LIMIT 5;
 
+-- Listing 16-16: Finding earthquakes with the most Did You Feel It? reports
 
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 9415-9416). Kindle Edition. 
 
-
+SELECT earthquake #>> '{properties, place}' AS place,
+	to_timestamp((earthquake #>> '{properties, time}')::bigint / 1000)
+		AT TIME ZONE 'UTC' AS time,
+	(earthquake #>> '{properties, mag}')::numeric AS magnitude,
+	(earthquake #>>  '{properties, felt}')::integer AS felt
+FROM earthquakes
+ORDER BY (earthquake #>> '{properties, felt}')::integer DESC NULLS LAST
+LIMIT 5;
 
 
 
