2d0fe511325cec45da48ad3f9bb0a77bacd3b449
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-29 14:56:18 -0400

    Add files via upload

--- Ch_16/ch_16.sql
+++ Ch_16/ch_16.sql
@@ -200,10 +200,18 @@ FROM earthquakes
 ORDER BY id
 LIMIT 5;
 
+-- Listing 16-18: Converting JSON location data to PostGIS geography
 
+-- Anthony DeBarros. 9781718501072 (Kindle Location 9466). Kindle Edition. 
 
-
-
+SELECT ST_SetSRID(
+	ST_MakePoint(
+		(earthquake #>> '{geometry, coordinates, 0}')::numeric,
+		(earthquake #>> '{geometry, coordinates, 1}')::numeric
+	),
+		4326)::geography AS earthquake_point
+FROM earthquakes
+ORDER BY id;
 
 
 
