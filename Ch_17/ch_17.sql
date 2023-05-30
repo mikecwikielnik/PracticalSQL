12ce4ed0cf72d62175acd01d0623de0e9f2b84bb
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-05-30 16:21:09 -0400

    Add files via upload

---
+++ Ch_17/ch_17.sql
@@ -0,0 +1,121 @@
+-- Chapter 17 
+-- Saving Time with Views, Functions, and Triggers
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 9739). Kindle Edition. 
+
+-- Listing 17-1: Creating a view that displays Nevada 2019 counties
+
+-- Anthony DeBarros. 9781718501072 (Kindle Location 9784). Kindle Edition. 
+
+CREATE OR REPLACE VIEW nevada_counties_pop_2019 AS
+SELECT county_name,
+	state_fips,
+	county_fips,
+	pop_est_2019
+FROM us_counties_pop_est_2019
+WHERE state_name = 'Nevada';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
