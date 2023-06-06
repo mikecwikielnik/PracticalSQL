1912802aa8b91058c2c6e26e16200ecea3843aa3
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-06-06 16:47:56 -0400

    Add files via upload

---
+++ Ch_19/ch_19.sql
@@ -0,0 +1,34 @@
+-- Chapter 19 
+-- Maintaining Your Database
+
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 11007-11008). Kindle Edition. 
+
+-- Listing 19-1: Creating a table to test vacuuming
+
+-- Anthony DeBarros. 9781718501072 (Kindle Locations 11047-11048). Kindle Edition. 
+
+CREATE TABLE vacuum_test (
+	integer_column integer
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
